26407113980b10b1616c4c9273b84102259cabf8
Burmau Garba <burmaud1@gmail.com>
2024-02-11 17:19:48 -0500

    minor twick to display

--- Lab1/display-processes.c
+++ Lab1/display-processes.c
@@ -11,7 +11,7 @@ main(void){
         printf("osh> ");
         fflush(stdout);
         // Make a child process
-        pid_t pid; 
+        pid_t pid, p, pp; 
         pid = fork();
         char user_input[MAX_LINE];
         scanf("%79[^\n]", user_input);
@@ -20,15 +20,16 @@ main(void){
             exit(-1);
         }
         else if(pid == 0){
+            p = getpid();
             char* argv[4] = {"bash", "-c", user_input, NULL};
             // Child process
             execvp(argv[0], argv);
             exit(1);
         }else{
+            pp = getpid();
+            printf("Child has completed printing the processes from execvp.\np: %d\npp: %d\n", p, pp);
             wait(NULL);
-            printf("Child has completed printing the processes from execvp.\n");
         }
-//        should_run = 0;
     }
     return 1;
 
